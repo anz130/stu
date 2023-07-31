377aa5e1e1ccf63c3ddd065716a5ce42a70800e9
anz130 <zxf130@hotmail.com>
2023-07-31 14:21:40 +0800

    2023-07-31 14:21:13添加bookdemo

--- stu_sbtWeb/src/main/java/sbt/StuSbtWebApplication.java
+++ stu_sbtWeb/src/main/java/sbt/StuSbtWebApplication.java
@@ -2,12 +2,21 @@ package sbt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import sbt.controller.BookController;
+
+
 
 @SpringBootApplication
 public class StuSbtWebApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StuSbtWebApplication.class, args);
+		ConfigurableApplicationContext ctx=SpringApplication.run(StuSbtWebApplication.class, args);
+		BookController bean=ctx.getBean(BookController.class);
+		System.out.println("bean==>"+bean);
+		User user=ctx.getBean(User.class);
+		System.out.println("userbena===>"+user);
 	}
 
 }
--- stu_sbtWeb/src/test/java/sbt/StuSbtWebApplicationTests.java
+++ stu_sbtWeb/src/test/java/sbt/StuSbtWebApplicationTests.java
@@ -1,13 +1,23 @@
 package sbt;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import sbt.dao.BookDao;
+
+//如测试类与引导类不在一个包下，需引用class@SpringBootTest(classes = StuSbtWebApplication.class)
 @SpringBootTest
 class StuSbtWebApplicationTests {
+	//1.注入你要测试的对象
+	@Autowired
+	private BookDao bookdao;
+	//2.执行要测试的对象对应的方法
 
 	@Test
 	void contextLoads() {
+		System.out.println("this is test...");
+		bookdao.save();
 	}
 
 }
